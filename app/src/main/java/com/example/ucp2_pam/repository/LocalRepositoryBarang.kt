a86813a564001201bb24def7dd60cc1fc1c32102
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-23 19:37:37 +0700

    Implementasikan fungsi insert, update, dan delete untuk Barang menggunakan barangDao

--- app/src/main/java/com/example/ucp2_pam/repository/LocalRepositoryBarang.kt
+++ app/src/main/java/com/example/ucp2_pam/repository/LocalRepositoryBarang.kt
@@ -1,7 +1,19 @@
 package com.example.ucp2_pam.repository
 
 import com.example.ucp2_pam.data.dao.BarangDao
+import com.example.ucp2_pam.data.entity.Barang
+
 class LocalRepositoryBarang(
     private val barangDao: BarangDao
 ) : RepositoryBarang {
+    override suspend fun insertBarang(barang: Barang){
+        barangDao.insertBarang(barang)
+    }
+
+    override suspend fun updateBarang(barang: Barang){
+        barangDao.updateBarang(barang)
+    }
+
+    override suspend fun deleteBarang(barang: Barang){
+        barangDao.deleteBarang(barang)
 }
