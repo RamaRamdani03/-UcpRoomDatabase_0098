f611b5411bdad18dd254d3f1c7a4ee05e0095f6e
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-24 06:21:45 +0700

    Menambahkan inisialisasi ViewModel dalam PenyediaViewModel.Factory untuk SuplierViewModel, SuplierHomeViewModel, HomeViewModel, BarangViewModel, HomeBarangViewModel, DetailBarangViewModel, dan UpdateBarangViewModel, dengan dependensi dari TokoApp

--- app/src/main/java/com/example/ucp2_pam/ui/viewModel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/ucp2_pam/ui/viewModel/PenyediaViewModel.kt
@@ -1,7 +1,53 @@
 package com.example.ucp2_pam.ui.viewModel
 
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.ucp2_pam.TokoApp
+import com.example.ucp2_pam.ui.viewModel.barang.BarangViewModel
+import com.example.ucp2_pam.ui.viewModel.barang.DetailBarangViewModel
+import com.example.ucp2_pam.ui.viewModel.barang.HomeBarangViewModel
+import com.example.ucp2_pam.ui.viewModel.barang.UpdateBarangViewModel
+import com.example.ucp2_pam.ui.viewModel.suplier.SuplierHomeViewModel
+import com.example.ucp2_pam.ui.viewModel.suplier.SuplierViewModel
+
 object PenyediaViewModel {
 
     val Factory = viewModelFactory {
+        initializer {
+            SuplierViewModel(
+                TokoApp().containerApp.repositorySuplier
+            )
+        }
+        initializer {
+            SuplierHomeViewModel(
+                TokoApp().containerApp.repositorySuplier
+            )
+        }
+        initializer {
+            HomeViewModel()
+        }
+        initializer {
+            BarangViewModel(
+                TokoApp().containerApp.repositoryBarang
+            )
+        }
+        initializer {
+            HomeBarangViewModel(
+                TokoApp().containerApp.repositoryBarang
+            )
+        }
+        initializer {
+            DetailBarangViewModel(
+                createSavedStateHandle(),
+                TokoApp().containerApp.repositoryBarang
+            )
+        }
+        initializer {
+            UpdateBarangViewModel(
+                createSavedStateHandle(),
+                TokoApp().containerApp.repositoryBarang
+            )
+        }
+    }
 }
