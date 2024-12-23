da2300e79c1f71cd39dec36e4b170ee2849874f3
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-23 20:12:30 +0700

    Tambah data class FormErrorState untuk menyimpan status error form dan validasi

--- app/src/main/java/com/example/ucp2_pam/ui/viewModel/suplier/SuplierViewModel.kt
+++ app/src/main/java/com/example/ucp2_pam/ui/viewModel/suplier/SuplierViewModel.kt
@@ -2,5 +2,18 @@ package com.example.ucp2_pam.ui.viewModel.suplier
 
 import androidx.lifecycle.ViewModel
 import com.example.ucp2_pam.repository.RepositorySuplier
+
 class SuplierViewModel (private val  repositorySuplier: RepositorySuplier) : ViewModel() {
+
+}
+
+data class FormErrorState(
+    val id: String? = null,
+    val nama: String? = null,
+    val kontak: String? = null,
+    val alamat: String? = null
+) {
+    fun isValid(): Boolean{
+        return id == null && nama == null && kontak == null && alamat == null
+    }
 }
