a73b478d4d1d985f4d7bf0549cc49323ff41c7ef
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-23 19:57:26 +0700

    Implementasikan StateFlow untuk mengelola state UI supplier di Home

--- app/src/main/java/com/example/ucp2_pam/ui/viewModel/suplier/HomeSuplierViewModel.kt
+++ app/src/main/java/com/example/ucp2_pam/ui/viewModel/suplier/HomeSuplierViewModel.kt
@@ -3,12 +3,23 @@ package com.example.ucp2_pam.ui.viewModel.suplier
 import androidx.lifecycle.ViewModel
 import com.example.ucp2_pam.data.entity.Suplier
 import com.example.ucp2_pam.repository.RepositorySuplier
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
 
 
 class SuplierHomeViewModel (
     private val repositorySuplier: RepositorySuplier
 ) : ViewModel() {
 
+    val homeUiState: StateFlow<HomeUiState> = repositorySuplier.getAllSuplier()
+        .filterNotNull()
+        .map {
+            HomeUiState (
+                listSuplier = it.toList(),
+                isLoading = false,
+            )
+        }
 }
 
 data class HomeUiState (
