493ef82ea9cd811baff1cf7299af782fa191d052
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-23 20:05:20 +0700

    stateIn() untuk mengelola state UI dengan viewModelScope dan penanganan berbagi state

--- app/src/main/java/com/example/ucp2_pam/ui/viewModel/suplier/HomeSuplierViewModel.kt
+++ app/src/main/java/com/example/ucp2_pam/ui/viewModel/suplier/HomeSuplierViewModel.kt
@@ -1,14 +1,17 @@
 package com.example.ucp2_pam.ui.viewModel.suplier
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp2_pam.data.entity.Suplier
 import com.example.ucp2_pam.repository.RepositorySuplier
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 
 
 class SuplierHomeViewModel (
@@ -36,6 +39,13 @@ class SuplierHomeViewModel (
                 )
             )
         }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUiState(
+                isLoading = true
+            )
+        )
 }
 
 data class HomeUiState (
