6331b8d95ae60447db62d3a1a5ab9b8228575e1a
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-23 23:16:34 +0700

    Menambahkan kelas HomeBarangViewModel dengan dependensi RepositoryBarang`

---
+++ app/src/main/java/com/example/ucp2_pam/ui/viewModel/barang/HomeBarangViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.ucp2_pam.ui.viewModel.barang
+
+import androidx.lifecycle.ViewModel
+import com.example.ucp2_pam.repository.RepositoryBarang
+
+class HomeBarangViewModel (
+    private val repositoryBarang: RepositoryBarang
+) : ViewModel() {
+
+}
