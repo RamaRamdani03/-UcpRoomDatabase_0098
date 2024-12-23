8159eadc6b1b8845c88416131ce14a965b4d8068
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-23 19:21:10 +0700

    Implementasikan fungsi insertSuplier() untuk menambah data pemasok

--- app/src/main/java/com/example/ucp2_pam/repository/LocalRepositorySuplier.kt
+++ app/src/main/java/com/example/ucp2_pam/repository/LocalRepositorySuplier.kt
@@ -1,9 +1,12 @@
 package com.example.ucp2_pam.repository
 
 import com.example.ucp2_pam.data.dao.SuplierDao
+import com.example.ucp2_pam.data.entity.Suplier
 
 class LocalRepositorySuplier (
     private val suplierDao: SuplierDao
 ) : RepositorySuplier {
-
+    override suspend fun insertSuplier(suplier: Suplier){
+        suplierDao.insertSuplier(suplier)
+    }
 }
