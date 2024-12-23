9bcbb34b371f95058409a640414eba4262a51669
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-23 23:54:13 +0700

    Tambahkan data class HomeUiStateBarang untuk merepresentasikan status UI Home Barang

--- app/src/main/java/com/example/ucp2_pam/ui/viewModel/barang/HomeBarangViewModel.kt
+++ app/src/main/java/com/example/ucp2_pam/ui/viewModel/barang/HomeBarangViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.ucp2_pam.ui.viewModel.barang
 
 import androidx.lifecycle.ViewModel
+import com.example.ucp2_pam.data.entity.Barang
 import com.example.ucp2_pam.repository.RepositoryBarang
 
 class HomeBarangViewModel (
@@ -8,3 +9,10 @@ class HomeBarangViewModel (
 ) : ViewModel() {
 
 }
+
+data class HomeUiStateBarang (
+    val listBarang: List<Barang> = listOf(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+)
