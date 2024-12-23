2f7d33656b7e5c81ec2f8ab7a41935b5b01c3d3c
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-23 19:18:39 +0700

    Tambah implementasi LocalRepositorySuplier dengan SuplierDao

---
+++ app/src/main/java/com/example/ucp2_pam/repository/LocalRepositorySuplier.kt
@@ -0,0 +1,9 @@
+package com.example.ucp2_pam.repository
+
+import com.example.ucp2_pam.data.dao.SuplierDao
+
+class LocalRepositorySuplier (
+    private val suplierDao: SuplierDao
+) : RepositorySuplier {
+
+}
