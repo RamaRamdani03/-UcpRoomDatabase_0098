d08671e5df6ddfceda575a56d69658db773aa6f8
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-24 00:02:05 +0700

    Menambahkan .onStart untuk menampilkan loading state awal pada StateFlow HomeUiStateBarang

--- app/src/main/java/com/example/ucp2_pam/ui/viewModel/barang/HomeBarangViewModel.kt
+++ app/src/main/java/com/example/ucp2_pam/ui/viewModel/barang/HomeBarangViewModel.kt
@@ -1,11 +1,17 @@
 package com.example.ucp2_pam.ui.viewModel.barang
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp2_pam.data.entity.Barang
 import com.example.ucp2_pam.repository.RepositoryBarang
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 
 class HomeBarangViewModel (
     private val repositoryBarang: RepositoryBarang
@@ -19,6 +25,10 @@ class HomeBarangViewModel (
                 isLoading = false,
             )
         }
+        .onStart {
+            emit(HomeUiStateBarang(isLoading = true))
+            delay(900)
+
 }
 
 data class HomeUiStateBarang (
