5dae8fd71aa028ec92cc0ba5e3c9089ca42fd436
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-23 19:55:17 +0700

    Tambah kelas SuplierHomeViewModel untuk mengelola data supplier di UI

--- app/src/main/java/com/example/ucp2_pam/ui/viewModel/suplier/HomeSuplierViewModel.kt
+++ app/src/main/java/com/example/ucp2_pam/ui/viewModel/suplier/HomeSuplierViewModel.kt
@@ -1,7 +1,15 @@
 package com.example.ucp2_pam.ui.viewModel.suplier
 
+import androidx.lifecycle.ViewModel
 import com.example.ucp2_pam.data.entity.Suplier
+import com.example.ucp2_pam.repository.RepositorySuplier
 
+
+class SuplierHomeViewModel (
+    private val repositorySuplier: RepositorySuplier
+) : ViewModel() {
+
+}
 
 data class HomeUiState (
     val listSuplier: List<Suplier> = listOf(),
