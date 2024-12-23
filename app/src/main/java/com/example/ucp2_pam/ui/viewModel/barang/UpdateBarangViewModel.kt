95657f4ade4197b472fa8bfcf6c8c48a7c2cf49e
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-24 01:05:35 +0700

    Menambahkan kelas UpdateBarangViewModel dengan dependency SavedStateHandle dan RepositoryBarang

---
+++ app/src/main/java/com/example/ucp2_pam/ui/viewModel/barang/UpdateBarangViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.ucp2_pam.ui.viewModel.barang
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.ucp2_pam.repository.RepositoryBarang
+
+class UpdateBarangViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val repositoryBarang: RepositoryBarang
+) : ViewModel() {
+
+}
