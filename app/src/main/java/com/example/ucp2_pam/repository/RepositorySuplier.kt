95d65d7da9997a93e82e67b6cf5a9b6fd53d7727
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-23 19:13:52 +0700

    Tambahkan metode untuk mengambil pemasok: getAllSuplier() dan getSuplier(id)

--- app/src/main/java/com/example/ucp2_pam/repository/RepositorySuplier.kt
+++ app/src/main/java/com/example/ucp2_pam/repository/RepositorySuplier.kt
@@ -1,10 +1,16 @@
 package com.example.ucp2_pam.repository
 
 import com.example.ucp2_pam.data.entity.Suplier
+import kotlinx.coroutines.flow.Flow
+
 interface RepositorySuplier {
     suspend fun insertSuplier(suplier: Suplier)
 
     suspend fun deleteSuplier(suplier: Suplier)
 
     suspend fun updateSuplier(suplier: Suplier)
+
+    fun getAllSuplier(): Flow<List<Suplier>>
+
+    fun getSuplier(id: String): Flow<Suplier>
 }
