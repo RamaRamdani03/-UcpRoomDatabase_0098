95eb5c0ce192b238d6fed0b61879759a405461fa
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-24 00:37:10 +0700

    Menambahkan data class FormErrorStateBarang untuk validasi kesalahan input

--- app/src/main/java/com/example/ucp2_pam/ui/viewModel/barang/BarangViewModel.kt
+++ app/src/main/java/com/example/ucp2_pam/ui/viewModel/barang/BarangViewModel.kt
@@ -6,3 +6,12 @@ import com.example.ucp2_pam.repository.RepositoryBarang
 class BarangViewModel (private val repositoryBarang: RepositoryBarang) : ViewModel() {
 
 }
+
+data class FormErrorStateBarang(
+    val id: String? = null,
+    val nama: String? = null,
+    val deskripsi: String? = null,
+    val harga: String? = null,
+    val stok: String? = null,
+    val NamaSuplier: String? = null,
+)
