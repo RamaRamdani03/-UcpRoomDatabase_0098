8da2f27ada64ce1759dc1d2767fa53faf4f13c38
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-24 03:22:01 +0700

    Menambahkan ItemDetailBarang untuk menampilkan detail lengkap barang dalam format yang lebih terstruktur menggunakan Card dan ComponentDetailBarang.

--- app/src/main/java/com/example/ucp2_pam/ui/view/barang/DetailBarangView.kt
+++ app/src/main/java/com/example/ucp2_pam/ui/view/barang/DetailBarangView.kt
@@ -1,8 +1,13 @@
 package com.example.ucp2_pam.ui.view.barang
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -10,7 +15,43 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.ucp2_pam.data.entity.Barang
+
+@Composable
+fun ItemDetailBarang(
+    modifier: Modifier = Modifier,
+    barang: Barang
+) {
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(top = 80.dp), // Menambahkan padding atas agar Card tidak terlalu menempel di bagian atas
+        colors = CardDefaults.cardColors(
+            contentColor = MaterialTheme.colorScheme.primaryContainer,
+            containerColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            ComponentDetailBarang(judul = "Id", isinya = barang.id)
+            Spacer(modifier = Modifier.padding(16.dp))
+
+            ComponentDetailBarang(judul = "Nama", isinya = barang.nama)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailBarang(judul = "Deskripsi", isinya = barang.deskripsi)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailBarang(judul = "Harga", isinya = barang.harga)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailBarang(judul = "Stok", isinya = barang.stok)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailBarang(judul = "Nama Suplier", isinya = barang.NamaSuplier)
+            Spacer(modifier = Modifier.padding(4.dp))
+        }
+    }
+}
 
 @Composable
 fun ComponentDetailBarang(
