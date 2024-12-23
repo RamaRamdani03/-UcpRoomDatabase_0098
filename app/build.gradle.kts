2238d50c7fab04e9d64a92be80b03f307ce2add3
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-23 17:51:20 +0700

    Menambahkan penamaan .TokoApp pada AndroidManifest.xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
-
     id("com.google.devtools.ksp")
 }
 
@@ -52,7 +51,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
