87a0d1f4b30a3aeb6a11ff6f86f751eb21e3d61c
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-24 03:00:44 +0700

    Menambahkan fungsi DeleteConfirmationDialog sebagai dialog konfirmasi penghapusan data di aplikasi

---
+++ app/src/main/java/com/example/ucp2_pam/ui/view/barang/DetailBarangView.kt
@@ -0,0 +1,13 @@
+package com.example.ucp2_pam.ui.view.barang
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+private fun DeleteConfirmationDialog(
+    onDeleteConfirm: () -> Unit,
+    onDeleteCancel: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+
+}
