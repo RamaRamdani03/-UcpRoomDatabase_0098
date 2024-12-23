b68f581e64bd855d5241d18ed40a78d65626c435
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-24 04:33:47 +0700

    Menambahkan composable DynamicSelectTextField untuk dropdown menu dengan opsi dinamis dan pengelolaan kesalahan.

---
+++ app/src/main/java/com/example/ucp2_pam/ui/costumwidget/Dropdown.kt
@@ -0,0 +1,17 @@
+package com.example.ucp2_pam.ui.costumwidget
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun DynamicSelectTextField(
+    selectedValue: String,
+    options: List<String>,
+    label: String,
+    onValueChangedEvent: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    isError: Boolean = false
+) {
+
+}
