8434577d52cb103ec4f17dcf4443e779b1674e81
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-23 20:03:30 +0700

    Tangani error dengan emit() untuk memperbarui status UI saat terjadi kesalahan

--- app/src/main/java/com/example/ucp2_pam/ui/viewModel/suplier/HomeSuplierViewModel.kt
+++ app/src/main/java/com/example/ucp2_pam/ui/viewModel/suplier/HomeSuplierViewModel.kt
@@ -1,17 +1,14 @@
 package com.example.ucp2_pam.ui.viewModel.suplier
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.example.ucp2_pam.data.entity.Suplier
 import com.example.ucp2_pam.repository.RepositorySuplier
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
-import kotlinx.coroutines.flow.stateIn
 
 
 class SuplierHomeViewModel (
@@ -29,6 +26,15 @@ class SuplierHomeViewModel (
         .onStart {
             emit(HomeUiState(isLoading = true))
             delay(900)
+        }
+        .catch {
+            emit(
+                HomeUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message?: "Terjadi Kesalahan"
+                )
+            )
         }
 }
 
