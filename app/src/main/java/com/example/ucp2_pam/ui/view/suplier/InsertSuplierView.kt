2460651209ceba2b3c11c0489616f0a028e97137
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-23 20:51:06 +0700

    Tambahkan OutlinedTextField untuk input ID suplier dengan validasi error

--- app/src/main/java/com/example/ucp2_pam/ui/view/suplier/InsertSuplierView.kt
+++ app/src/main/java/com/example/ucp2_pam/ui/view/suplier/InsertSuplierView.kt
@@ -2,11 +2,14 @@ package com.example.ucp2_pam.ui.view.suplier
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.example.ucp2_pam.ui.viewModel.suplier.FormErrorState
 import com.example.ucp2_pam.ui.viewModel.suplier.SuplierEvent
-
+import kotlinx.coroutines.launch
 
 @Composable
 fun FormSuplier(
@@ -20,5 +23,19 @@ fun FormSuplier(
         modifier = modifier.fillMaxWidth()
     ) {
 
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = suplierEvent.id,
+            onValueChange = {
+                onValueChange(suplierEvent.copy(id = it))
+            },
+            label = { Text("id") },
+            isError = errorState.id != null,
+            placeholder = { Text("Masukkan id") },
+        )
+        Text(
+            text = errorState.id ?: "",
+            color = Color.Red
+        )
     }
 }
