fa457bb4dc6638f99bf5af9dd4fdc43e96769176
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-24 01:29:27 +0700

    Composable function CardSuplier untuk menampilkan kartu suplier dengan tindakan klik

---
+++ app/src/main/java/com/example/ucp2_pam/ui/view/suplier/HomeSuplierView.kt
@@ -0,0 +1,14 @@
+package com.example.ucp2_pam.ui.view.suplier
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.ucp2_pam.data.entity.Suplier
+
+@Composable
+fun CardSuplier(
+    suplier: Suplier,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = { }
+) {
+
+}
