bbccabc11fdb993b3e798f400346808235d188fa
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-23 19:36:16 +0700

    Mengubah penamaan RepositoryBr menjadi RepositoryBarang

---
+++ app/src/main/java/com/example/ucp2_pam/repository/LocalRepositoryBarang.kt
@@ -0,0 +1,7 @@
+package com.example.ucp2_pam.repository
+
+import com.example.ucp2_pam.data.dao.BarangDao
+class LocalRepositoryBarang(
+    private val barangDao: BarangDao
+) : RepositoryBarang {
+}
