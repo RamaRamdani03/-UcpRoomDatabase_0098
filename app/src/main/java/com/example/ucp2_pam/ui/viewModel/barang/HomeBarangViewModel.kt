bfad7374088ccb8b2e60a6650e7b4e690dd66abe
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-23 23:57:17 +0700

    Inisialisasi StateFlow untuk HomeUiStateBarang dengan data dari repositoryBarang

--- app/src/main/java/com/example/ucp2_pam/ui/viewModel/barang/HomeBarangViewModel.kt
+++ app/src/main/java/com/example/ucp2_pam/ui/viewModel/barang/HomeBarangViewModel.kt
@@ -3,11 +3,15 @@ package com.example.ucp2_pam.ui.viewModel.barang
 import androidx.lifecycle.ViewModel
 import com.example.ucp2_pam.data.entity.Barang
 import com.example.ucp2_pam.repository.RepositoryBarang
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
 
 class HomeBarangViewModel (
     private val repositoryBarang: RepositoryBarang
 ) : ViewModel() {
 
+    val homeUiStateBarang: StateFlow<HomeUiStateBarang> = repositoryBarang.getAllBarang()
+        .filterNotNull()
 }
 
 data class HomeUiStateBarang (
