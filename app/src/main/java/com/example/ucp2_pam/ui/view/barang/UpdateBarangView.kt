f0b6301084df321854d928a4fd46dad35e19f790
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-24 02:51:26 +0700

    LaunchedEffect untuk menampilkan Snackbar dengan pesan dari updateUiStateBarang dan mereset pesan setelah ditampilkan

--- app/src/main/java/com/example/ucp2_pam/ui/view/barang/UpdateBarangView.kt
+++ app/src/main/java/com/example/ucp2_pam/ui/view/barang/UpdateBarangView.kt
@@ -1,6 +1,11 @@
 package com.example.ucp2_pam.ui.view.barang
 
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.ucp2_pam.ui.viewModel.PenyediaViewModel
@@ -13,5 +18,19 @@ fun UpdateBarangView(
     modifier: Modifier = Modifier,
     viewModel: UpdateBarangViewModel = viewModel (factory = PenyediaViewModel.Factory)
 ) {
+    val uiSateBarang = viewModel.updateUiStateBarang
+    val snackbarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
 
+    LaunchedEffect (uiSateBarang.snackBarMessage) {
+        println("LaunchedEffect triggered")
+        uiSateBarang.snackBarMessage?.let { message ->
+            println("Launching coroutine for snackbar")
+            snackbarHostState.showSnackbar(
+                message = message,
+                duration = SnackbarDuration.Long,
+            )
+            viewModel.resetSnackBarMessage()
+        }
+    }
 }
