69fe35af9eb56ba1f056cdcc318ff9de20c46d8e
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-23 18:03:19 +0700

    Menambahkan entitas Barang dan Suplier pada TokoDatabase

--- app/src/main/java/com/example/ucp2_pam/data/database/TokoDatabase.kt
+++ app/src/main/java/com/example/ucp2_pam/data/database/TokoDatabase.kt
@@ -1,4 +1,11 @@
 package com.example.ucp2_pam.data.database
 
-class TokoDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.ucp2_pam.data.entity.Barang
+import com.example.ucp2_pam.data.entity.Suplier
+
+@Database(entities = [Barang::class, Suplier::class], version = 1, exportSchema = false)
+abstract class TokoDatabase : RoomDatabase(){
+
 }
