6eec604a102e0ff258bc2423ddaae141bf05bfe5
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-23 20:02:06 +0700

    Menambahkan tutup kurung kuralwal

--- app/src/main/java/com/example/ucp2_pam/ui/viewModel/suplier/HomeSuplierViewModel.kt
+++ app/src/main/java/com/example/ucp2_pam/ui/viewModel/suplier/HomeSuplierViewModel.kt
@@ -1,13 +1,17 @@
 package com.example.ucp2_pam.ui.viewModel.suplier
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp2_pam.data.entity.Suplier
 import com.example.ucp2_pam.repository.RepositorySuplier
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 
 
 class SuplierHomeViewModel (
@@ -25,7 +29,7 @@ class SuplierHomeViewModel (
         .onStart {
             emit(HomeUiState(isLoading = true))
             delay(900)
-
+        }
 }
 
 data class HomeUiState (
