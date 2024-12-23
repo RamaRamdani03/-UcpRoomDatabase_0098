22b4481ffe0f0311fd55b986a21994d1748d5284
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-23 18:06:30 +0700

    Menambahkan method abstract untuk akses BarangDao dan SuplierDao

--- app/src/main/java/com/example/ucp2_pam/data/database/TokoDatabase.kt
+++ app/src/main/java/com/example/ucp2_pam/data/database/TokoDatabase.kt
@@ -1,11 +1,18 @@
 package com.example.ucp2_pam.data.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.example.ucp2_pam.data.dao.BarangDao
+import com.example.ucp2_pam.data.dao.SuplierDao
 import com.example.ucp2_pam.data.entity.Barang
 import com.example.ucp2_pam.data.entity.Suplier
 
 @Database(entities = [Barang::class, Suplier::class], version = 1, exportSchema = false)
 abstract class TokoDatabase : RoomDatabase(){
 
+    abstract fun barangDao(): BarangDao
+
+    abstract fun supplierDao(): SuplierDao
 }
