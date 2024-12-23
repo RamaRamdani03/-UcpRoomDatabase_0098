d0935709e5cbb82a2ec55299ec628394f827cc81
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-23 19:38:49 +0700

    Implementasikan fungsi getAllBarang() dan getBarang(id) untuk mengambil data barang

--- app/src/main/java/com/example/ucp2_pam/repository/LocalRepositoryBarang.kt
+++ app/src/main/java/com/example/ucp2_pam/repository/LocalRepositoryBarang.kt
@@ -2,6 +2,7 @@ package com.example.ucp2_pam.repository
 
 import com.example.ucp2_pam.data.dao.BarangDao
 import com.example.ucp2_pam.data.entity.Barang
+import kotlinx.coroutines.flow.Flow
 
 class LocalRepositoryBarang(
     private val barangDao: BarangDao
@@ -16,4 +17,11 @@ class LocalRepositoryBarang(
 
     override suspend fun deleteBarang(barang: Barang){
         barangDao.deleteBarang(barang)
+    }
+
+    override fun getAllBarang(): Flow<List<Barang>> =
+        barangDao.getAllBarang()
+
+    override fun getBarang(id: String): Flow<Barang> =
+        barangDao.getBarang(id)
 }
