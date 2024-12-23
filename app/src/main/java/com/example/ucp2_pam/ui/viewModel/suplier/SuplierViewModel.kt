de35fc09e7b4bcabea7f57b6ff0e0285b9ceb5c6
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-23 20:17:18 +0700

    Tambah data class SuplierUiState untuk menyimpan status UI supplier dan validasi form

--- app/src/main/java/com/example/ucp2_pam/ui/viewModel/suplier/SuplierViewModel.kt
+++ app/src/main/java/com/example/ucp2_pam/ui/viewModel/suplier/SuplierViewModel.kt
@@ -1,8 +1,13 @@
 package com.example.ucp2_pam.ui.viewModel.suplier
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp2_pam.data.entity.Suplier
 import com.example.ucp2_pam.repository.RepositorySuplier
+import kotlinx.coroutines.launch
 
 class SuplierViewModel (private val  repositorySuplier: RepositorySuplier) : ViewModel() {
 
@@ -32,4 +37,10 @@ data class SuplierEvent(
     val nama: String = "",
     val kontak: String = "",
     val alamat: String = "",
+)
+
+data class SuplierUiState(
+    val suplierEvent: SuplierEvent = SuplierEvent(),
+    val isEntryValid: FormErrorState = FormErrorState(),
+    val snackBarMessage: String? = null,
 )
