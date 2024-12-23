9f754a2a22f856151fd01cd60b783988719acfbe
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-24 03:42:48 +0700

    Menambahkan DetailBarangView untuk menampilkan detail barang lengkap dengan fitur edit dan delete, serta integrasi dengan viewModel untuk pengambilan data dan aksi terkait.

--- app/src/main/java/com/example/ucp2_pam/ui/view/barang/DetailBarangView.kt
+++ app/src/main/java/com/example/ucp2_pam/ui/view/barang/DetailBarangView.kt
@@ -6,15 +6,21 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -25,9 +31,65 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.ucp2_pam.data.entity.Barang
+import com.example.ucp2_pam.ui.costumwidget.TopAppBar
+import com.example.ucp2_pam.ui.viewModel.PenyediaViewModel
+import com.example.ucp2_pam.ui.viewModel.barang.DetailBarangViewModel
 import com.example.ucp2_pam.ui.viewModel.barang.DetailUiStateBarang
 import com.example.ucp2_pam.ui.viewModel.barang.toBarangEntity
+
+@Composable
+fun DetailBarangView(
+    modifier: Modifier = Modifier,
+    viewModel: DetailBarangViewModel = viewModel(factory = PenyediaViewModel.Factory),
+    onBack: () -> Unit = { },
+    onEditClick: (String) -> Unit = { },
+    onDeleteClick: () -> Unit = { }
+) {
+    Scaffold (
+        topBar = {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 24.dp, bottom = 8.dp),
+                contentAlignment = Alignment.Center
+            ) {
+            TopAppBar(
+                judul = "Detail Barang",
+                showBackButton = true,
+                onBack = onBack
+            ) }
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    onEditClick(viewModel.detailUiState.value.detailUiEvent.id)
+                },
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(16.dp)
+            ) {
+               Icon(
+                   imageVector = Icons.Default.Edit,
+                   contentDescription = ""
+               )
+            }
+        }
+    ) { innerPadding ->
+        val detailUiState by viewModel.detailUiState.collectAsState()
+
+        BodyDetailBarang(
+            modifier = Modifier.padding(innerPadding),
+            detailUiState = detailUiState,
+            onDeleteClick = {
+                viewModel.deleteBarang()
+                onDeleteClick()
+            }
+        )
+
+    }
+
+}
 
 @Composable
 fun BodyDetailBarang(
