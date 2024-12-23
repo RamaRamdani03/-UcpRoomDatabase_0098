6f36d63013022c6d073eab0161adbfd813f2ac8a
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-23 17:56:59 +0700

    Menambahkan query untuk mengambil semua suplier dan mengurutkannya berdasarkan nama secara ascending

--- app/src/main/java/com/example/ucp2_pam/data/dao/SuplierDao.kt
+++ app/src/main/java/com/example/ucp2_pam/data/dao/SuplierDao.kt
@@ -3,8 +3,10 @@ package com.example.ucp2_pam.data.dao
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.Query
 import androidx.room.Update
 import com.example.ucp2_pam.data.entity.Suplier
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface SuplierDao{
@@ -16,4 +18,7 @@ interface SuplierDao{
 
     @Delete
     suspend fun deleteSuplier(suplier: Suplier)
+
+    @Query("SELECT * FROM suplier ORDER BY nama ASC")
+    fun getAllSuplier(): Flow<List<Suplier>>
 }
