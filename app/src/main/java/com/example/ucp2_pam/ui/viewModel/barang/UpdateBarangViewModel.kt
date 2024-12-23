99a20a72d0d5d27752eabe56825edc6dc875c399
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-24 01:10:13 +0700

    Inisialisasi _id dan memuat data Barang ke updateUiStateBarang berdasarkan ID yang diberikan

--- app/src/main/java/com/example/ucp2_pam/ui/viewModel/barang/UpdateBarangViewModel.kt
+++ app/src/main/java/com/example/ucp2_pam/ui/viewModel/barang/UpdateBarangViewModel.kt
@@ -5,8 +5,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp2_pam.data.entity.Barang
 import com.example.ucp2_pam.repository.RepositoryBarang
+import com.example.ucp2_pam.ui.navigasi.DestinasiUpdateBarang
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
 class UpdateBarangViewModel (
     savedStateHandle: SavedStateHandle,
@@ -15,6 +20,17 @@ class UpdateBarangViewModel (
 
     var updateUiStateBarang by mutableStateOf(BarangUiState())
         private set
+
+    private val _id: String = checkNotNull(savedStateHandle[DestinasiUpdateBarang.id])
+
+    init {
+        viewModelScope.launch {
+            updateUiStateBarang = repositoryBarang.getBarang(_id)
+                .filterNotNull()
+                .first()
+                .toUiStateBarang()
+        }
+    }
 }
 
 fun Barang.toUiStateBarang() : BarangUiState = BarangUiState (
