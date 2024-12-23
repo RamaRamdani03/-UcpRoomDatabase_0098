9c67a5fa55725df454494742c4d95cae8a069f14
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-23 19:32:04 +0700

    Tambahkan fungsi untuk mengambil semua barang dan barang berdasarkan ID

--- app/src/main/java/com/example/ucp2_pam/repository/RepositoryBarang.kt
+++ app/src/main/java/com/example/ucp2_pam/repository/RepositoryBarang.kt
@@ -1,6 +1,7 @@
 package com.example.ucp2_pam.repository
 
 import com.example.ucp2_pam.data.entity.Barang
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryBarang {
     suspend fun insertBarang(barang: Barang)
@@ -8,4 +9,8 @@ interface RepositoryBarang {
     suspend fun deleteBarang(barang: Barang)
 
     suspend fun updateBarang(barang: Barang)
+
+    fun getAllBarang(): Flow<List<Barang>>
+
+    fun getBarang(id: String): Flow<Barang>
 }
