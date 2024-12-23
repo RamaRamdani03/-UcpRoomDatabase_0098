d2c0b2e6ab6cd19a4b2d7fc45a15630315450668
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-23 18:17:51 +0700

    Menambahkan entitas Suplier dengan tabel "suplier"

--- app/src/main/java/com/example/ucp2_pam/data/entity/Suplier.kt
+++ app/src/main/java/com/example/ucp2_pam/data/entity/Suplier.kt
@@ -1,13 +1,9 @@
 package com.example.ucp2_pam.data.entity
 
+
 import androidx.room.Entity
-import androidx.room.PrimaryKey
 
 @Entity(tableName = "suplier")
 data class Suplier(
-    @PrimaryKey
-    val id: String,
-    val nama: String,
-    val kontak: String,
-    val alamat: String,
+
 )
