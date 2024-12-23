5d4a16c5e0039994786eb42866d9546a61fb17a0
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-23 19:30:34 +0700

    Tambahkan fungsi suspend untuk operasi insert, delete, dan update pada Barang

--- app/src/main/java/com/example/ucp2_pam/repository/RepositoryBarang.kt
+++ app/src/main/java/com/example/ucp2_pam/repository/RepositoryBarang.kt
@@ -1,5 +1,11 @@
 package com.example.ucp2_pam.repository
 
-interface RepositoryBarang {
+import com.example.ucp2_pam.data.entity.Barang
 
+interface RepositoryBarang {
+    suspend fun insertBarang(barang: Barang)
+
+    suspend fun deleteBarang(barang: Barang)
+
+    suspend fun updateBarang(barang: Barang)
 }
