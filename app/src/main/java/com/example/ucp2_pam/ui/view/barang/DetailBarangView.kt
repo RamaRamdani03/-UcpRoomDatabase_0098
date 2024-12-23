fedfc1f46353734fae2159558e50f2fa5492f333
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-24 03:30:43 +0700

    BodyDetailBarang untuk menampilkan detail barang beserta konfirmasi penghapusan data dengan DeleteConfirmationDialog

--- app/src/main/java/com/example/ucp2_pam/ui/view/barang/DetailBarangView.kt
+++ app/src/main/java/com/example/ucp2_pam/ui/view/barang/DetailBarangView.kt
@@ -1,16 +1,24 @@
 package com.example.ucp2_pam.ui.view.barang
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -18,6 +26,73 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.ucp2_pam.data.entity.Barang
+import com.example.ucp2_pam.ui.viewModel.barang.DetailUiStateBarang
+import com.example.ucp2_pam.ui.viewModel.barang.toBarangEntity
+
+@Composable
+fun BodyDetailBarang(
+    modifier: Modifier = Modifier,
+    detailUiState: DetailUiStateBarang = DetailUiStateBarang(),
+    onDeleteClick: () -> Unit = { }
+) {
+    var deleteConfirmationRequired by rememberSaveable { mutableStateOf(false) }
+    when {
+        detailUiState.isLoading -> {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
+        }
+
+        detailUiState.isUiEventNotEmpty -> {
+            Column (
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                ItemDetailBarang(
+                    barang = detailUiState.detailUiEvent.toBarangEntity(),
+                    modifier = Modifier
+
+                )
+                Spacer(modifier = Modifier.padding(8.dp))
+                Button(
+                    onClick = {
+                        deleteConfirmationRequired = true
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                   Text("Delete")
+                }
+
+                if (deleteConfirmationRequired) {
+                    DeleteConfirmationDialog(
+                        onDeleteConfirm = {
+                            deleteConfirmationRequired = false
+                            onDeleteClick()
+                        },
+                        onDeleteCancel = { deleteConfirmationRequired = false },
+                        modifier =Modifier.padding(8.dp)
+                    )
+                }
+            }
+        }
+
+        detailUiState.isUiEventNotEmpty -> {
+            Box(
+                modifier = Modifier.fillMaxWidth(),
+                contentAlignment = Alignment.BottomCenter
+            ) {
+                Text(
+                    text = "Data tidak ditemukan",
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+    }
+}
 
 @Composable
 fun ItemDetailBarang(
@@ -27,7 +102,7 @@ fun ItemDetailBarang(
     Card(
         modifier = modifier
             .fillMaxWidth()
-            .padding(top = 80.dp), // Menambahkan padding atas agar Card tidak terlalu menempel di bagian atas
+            .padding(top = 80.dp),
         colors = CardDefaults.cardColors(
             contentColor = MaterialTheme.colorScheme.primaryContainer,
             containerColor = MaterialTheme.colorScheme.onPrimaryContainer
