7d5fcfcedb95591f6ed2dbee921924c8e39d1691
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-23 20:09:29 +0700

    Tambahkan kelas SuplierViewModel untuk mengelola logika bisnis terkait supplier

---
+++ app/src/main/java/com/example/ucp2_pam/ui/viewModel/suplier/SuplierViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.ucp2_pam.ui.viewModel.suplier
+
+import androidx.lifecycle.ViewModel
+import com.example.ucp2_pam.repository.RepositorySuplier
+class SuplierViewModel (private val  repositorySuplier: RepositorySuplier) : ViewModel() {
+}
