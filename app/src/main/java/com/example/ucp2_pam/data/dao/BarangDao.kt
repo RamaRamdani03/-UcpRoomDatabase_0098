a52586f2f78f3f4bcf6d9fb6a65dcc64cf42c6e0
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-23 17:55:03 +0700

    Menambahkan query untuk mengambil semua barang dan mengurutkannya berdasarkan nama secara ascending

--- app/src/main/java/com/example/ucp2_pam/data/dao/BarangDao.kt
+++ app/src/main/java/com/example/ucp2_pam/data/dao/BarangDao.kt
@@ -3,8 +3,10 @@ package com.example.ucp2_pam.data.dao
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.Query
 import androidx.room.Update
 import com.example.ucp2_pam.data.entity.Barang
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface BarangDao {
@@ -16,4 +18,7 @@ interface BarangDao {
 
     @Delete
     suspend fun deleteBarang(barang: Barang)
+
+    @Query("SELECT * FROM barang ORDER BY nama ASC")
+    fun getAllBarang(): Flow<List<Barang>>
 }
