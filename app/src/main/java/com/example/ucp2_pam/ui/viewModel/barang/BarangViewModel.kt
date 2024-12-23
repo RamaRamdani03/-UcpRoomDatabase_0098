53e5795076d3794fe9dcfacc000b2ad332253357
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-24 00:34:55 +0700

    Menambahkan inisialisasi BarangViewModel dengan dependency RepositoryBarang

---
+++ app/src/main/java/com/example/ucp2_pam/ui/viewModel/barang/BarangViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.ucp2_pam.ui.viewModel.barang
+
+import androidx.lifecycle.ViewModel
+import com.example.ucp2_pam.repository.RepositoryBarang
+
+class BarangViewModel (private val repositoryBarang: RepositoryBarang) : ViewModel() {
+
+}
