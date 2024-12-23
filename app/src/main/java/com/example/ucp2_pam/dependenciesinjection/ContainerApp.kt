74a079f3b221318b6883bda433bbb08cb727c653
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-23 18:23:26 +0700

    Menambahkan class ContainerApp yang mengimplementasikan InterfaceContainerApp dan menginisialisasi repositoryBarang serta repositorySuplier

--- app/src/main/java/com/example/ucp2_pam/dependenciesinjection/ContainerApp.kt
+++ app/src/main/java/com/example/ucp2_pam/dependenciesinjection/ContainerApp.kt
@@ -1,10 +1,22 @@
 package com.example.ucp2_pam.dependenciesinjection
 
-
+import android.content.Context
+import com.example.ucp2_pam.data.database.TokoDatabase
+import com.example.ucp2_pam.repository.LocalRepositoryBarang
+import com.example.ucp2_pam.repository.LocalRepositorySuplier
 import com.example.ucp2_pam.repository.RepositoryBarang
 import com.example.ucp2_pam.repository.RepositorySuplier
 
 interface InterfaceContainerApp {
     val repositoryBarang: RepositoryBarang
     val repositorySuplier: RepositorySuplier
+}
+
+class ContainerApp (private val context: Context) : InterfaceContainerApp{
+    override val repositoryBarang:RepositoryBarang by lazy {
+        LocalRepositoryBarang(TokoDatabase.getDatabase(context).barangDao())
+    }
+    override val repositorySuplier:RepositorySuplier by lazy {
+        LocalRepositorySuplier(TokoDatabase.getDatabase(context).supplierDao())
+    }
 }
