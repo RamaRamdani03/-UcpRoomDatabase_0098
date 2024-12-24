1dc7f06b937fed190c1f7c858bab22b68db4a061
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-24 08:21:41 +0700

    Memanggil fungsi untuk menjalankan aplikasi

--- app/src/main/java/com/example/ucp2_pam/MainActivity.kt
+++ app/src/main/java/com/example/ucp2_pam/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.ucp2_pam.ui.navigasi.PengelolaanHalaman
 import com.example.ucp2_pam.ui.theme.Ucp2_pamTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Ucp2_pamTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    PengelolaanHalaman(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
