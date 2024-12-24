e792b74da5fc945964c540e8491d14a53ff7a345
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-24 07:28:45 +0700

    Menambahkan interface AlamatNavigasi dan beberapa objek destinasi untuk navigasi aplikasi

--- app/src/main/java/com/example/ucp2_pam/ui/navigasi/AlamatNavigasi.kt
+++ app/src/main/java/com/example/ucp2_pam/ui/navigasi/AlamatNavigasi.kt
@@ -1,4 +1,25 @@
 package com.example.ucp2_pam.ui.navigasi
 
 interface AlamatNavigasi {
+    val route: String
+}
+
+object DestinasiHome : AlamatNavigasi {
+    override val route = "home"
+}
+
+object DestinasiHomeBarang : AlamatNavigasi {
+    override val route = "homebarang"
+}
+
+object DestinasiHomeSuplier : AlamatNavigasi {
+    override val route = "homesuplier"
+}
+
+object DestinasiAddSuplier : AlamatNavigasi {
+    override val route = "addsuplier"
+}
+
+object DestinasAddBarang : AlamatNavigasi {
+    override val route = "addbarang"
 }
