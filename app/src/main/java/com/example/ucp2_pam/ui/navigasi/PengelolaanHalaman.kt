9c5763aade69f9f2944b98932bf5924e11a7455c
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-24 07:36:17 +0700

    Menambahkan fungsi PengelolaanHalaman sebagai Composable untuk pengelolaan halaman dengan NavController

--- app/src/main/java/com/example/ucp2_pam/ui/navigasi/PengelolaanHalaman.kt
+++ app/src/main/java/com/example/ucp2_pam/ui/navigasi/PengelolaanHalaman.kt
@@ -1,2 +1,14 @@
 package com.example.ucp2_pam.ui.navigasi
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun PengelolaanHalaman (
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier = Modifier
+) {
+
+}
