602117913af5e77f762d32752752013a2624223d
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-24 07:13:44 +0700

    Menambahkan komponen TopAppBar dengan dukungan tombol kembali opsional dan judul yang dapat disesuaikan

--- app/src/main/java/com/example/ucp2_pam/ui/costumwidget/TopAppBar.kt
+++ app/src/main/java/com/example/ucp2_pam/ui/costumwidget/TopAppBar.kt
@@ -1,2 +1,58 @@
 package com.example.ucp2_pam.ui.costumwidget
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun TopAppBar(
+    onBack: () -> Unit,
+    showBackButton: Boolean = true,
+    judul: String,
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        if (showBackButton) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                IconButton(
+                    onClick = onBack,
+                    modifier = Modifier.align(Alignment.CenterVertically)
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.ArrowBack,
+                        contentDescription = "Back"
+                    )
+                }
+
+                Spacer(modifier = Modifier.weight(1f))
+            }
+        }
+
+        Text(
+            text = judul,
+            fontSize = 25.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.align(Alignment.Center)
+        )
+    }
+}
