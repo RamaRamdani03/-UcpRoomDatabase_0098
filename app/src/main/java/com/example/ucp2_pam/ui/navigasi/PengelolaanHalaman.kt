d3ae0cad21a1ae4520ae74af792f2375548928ea
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-24 08:21:08 +0700

    Navigasi untuk rute DestinasiDetailBarang, DestinasiUpdateBarang, dan DestinasiAddSuplier dengan penanganan argumen, aksi onBack, onEditClick, serta rute untuk menambahkan suplier baru.

--- app/src/main/java/com/example/ucp2_pam/ui/navigasi/PengelolaanHalaman.kt
+++ app/src/main/java/com/example/ucp2_pam/ui/navigasi/PengelolaanHalaman.kt
@@ -4,12 +4,17 @@ import HomeNavbar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.ucp2_pam.ui.view.barang.DetailBarangView
 import com.example.ucp2_pam.ui.view.barang.HomeBarangView
 import com.example.ucp2_pam.ui.view.barang.InsertBarangView
+import com.example.ucp2_pam.ui.view.barang.UpdateBarangView
 import com.example.ucp2_pam.ui.view.suplier.HomeSuplierView
+import com.example.ucp2_pam.ui.view.suplier.InsertSuplierView
 
 @Composable
 fun PengelolaanHalaman (
@@ -64,6 +69,62 @@ fun PengelolaanHalaman (
                    navController.popBackStack()
                },
                onNavigate = {  },
+               modifier = modifier
+           )
+       }
+
+       composable(
+           DestinasiDetailBarang.routesWithArg,
+           arguments = listOf(
+               navArgument(DestinasiDetailBarang.id) {
+                   type = NavType.StringType
+               }
+           )
+       ) {
+           val id = it.arguments?.getString(DestinasiDetailBarang.id)
+           id?.let { id ->
+               DetailBarangView(
+                   onBack = {
+                       navController.popBackStack()
+                   },
+                   onEditClick = {
+                       navController.navigate("${DestinasiUpdateBarang.route}/$it")
+                   },
+                   modifier = modifier,
+                   onDeleteClick = {
+                       navController.popBackStack()
+                   }
+               )
+           }
+       }
+
+       composable(
+           DestinasiUpdateBarang.routesWithArg,
+           arguments = listOf(
+               navArgument(DestinasiUpdateBarang.id) {
+                   type = NavType.StringType
+               }
+           )
+       ) {
+           UpdateBarangView(
+               onBack = {
+                   navController.popBackStack()
+               },
+               onNavigate = {
+                   navController.popBackStack()
+               },
+               modifier = modifier,
+           )
+       }
+
+       composable(
+           route = DestinasiAddSuplier.route
+       ) {
+           InsertSuplierView(
+               onBack = {
+                   navController.popBackStack()
+               },
+               onNavigate = { },
                modifier = modifier
            )
        }
