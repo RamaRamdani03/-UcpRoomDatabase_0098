b70d95b85f1f3de7f550c642c28ccac73ebb5efd
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-24 07:56:31 +0700

    Navigasi untuk HomeBarangView, HomeSuplierView, dan InsertBarangView dengan pengaturan onBack, onDetailBarangClick, dan onAddBarangClick

--- app/src/main/java/com/example/ucp2_pam/ui/navigasi/PengelolaanHalaman.kt
+++ app/src/main/java/com/example/ucp2_pam/ui/navigasi/PengelolaanHalaman.kt
@@ -7,6 +7,9 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.ucp2_pam.ui.view.barang.HomeBarangView
+import com.example.ucp2_pam.ui.view.barang.InsertBarangView
+import com.example.ucp2_pam.ui.view.suplier.HomeSuplierView
 
 @Composable
 fun PengelolaanHalaman (
@@ -17,14 +20,51 @@ fun PengelolaanHalaman (
        navController = navController,
        startDestination = DestinasiHome.route
    ) {
+      composable(
+          route = DestinasiHome.route
+      ) {
+          HomeNavbar(
+              onAddBarang = { navController.navigate(DestinasAddBarang.route) },
+              onAddSuplier = { navController.navigate(DestinasiAddSuplier.route) },
+              onHomeBarang = { navController.navigate(DestinasiHomeBarang.route) },
+              onHomeSuplier = { navController.navigate(DestinasiHomeSuplier.route) },
+          )
+      }
        composable(
-           route = DestinasiHome.route
+           route = DestinasiHomeBarang.route
        ) {
-           HomeNavbar(
-               onAddBarang = { navController.navigate(DestinasAddBarang.route) },
-               onAddSuplier = { navController.navigate(DestinasiAddSuplier.route) },
-               onHomeBarang = { navController.navigate(DestinasiHomeBarang.route) },
-               onHomeSuplier = { navController.navigate(DestinasiHomeSuplier.route) },
+           HomeBarangView (
+               onDetailBarangClick = {
+                   id ->
+                   navController.navigate("${DestinasiDetailBarang.route}/$id")
+                   println("PengelolaanHalaman: id_ = $id")
+               },
+               onAddBarangClick = { navController.navigate(DestinasAddBarang.route) },
+               onBack = {
+                   navController.popBackStack()
+               },
+               modifier = Modifier
+           )
+       }
+       composable(
+           route = DestinasiHomeSuplier.route
+       ) {
+           HomeSuplierView(
+               onBack = {
+                   navController.popBackStack()
+               },
+               modifier = Modifier
+           )
+       }
+       composable(
+           route = DestinasAddBarang.route
+       ) {
+           InsertBarangView(
+               onBack = {
+                   navController.popBackStack()
+               },
+               onNavigate = {  },
+               modifier = modifier
            )
        }
    }
