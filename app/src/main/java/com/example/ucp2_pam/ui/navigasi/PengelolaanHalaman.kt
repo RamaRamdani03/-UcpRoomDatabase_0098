b22a5bf818f917d3d8ad22b5b7dcf7fb7a6cf246
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-24 07:38:18 +0700

    konfigurasi NavHost dengan navigasi ke halaman HomeNavbar dan destinasi terkait.

--- app/src/main/java/com/example/ucp2_pam/ui/navigasi/PengelolaanHalaman.kt
+++ app/src/main/java/com/example/ucp2_pam/ui/navigasi/PengelolaanHalaman.kt
@@ -1,8 +1,11 @@
 package com.example.ucp2_pam.ui.navigasi
 
+import HomeNavbar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 
 @Composable
@@ -10,5 +13,19 @@ fun PengelolaanHalaman (
     navController: NavHostController = rememberNavController(),
     modifier: Modifier = Modifier
 ) {
-
+   NavHost(
+       navController = navController,
+       startDestination = DestinasiHome.route
+   ) {
+       composable(
+           route = DestinasiHome.route
+       ) {
+           HomeNavbar(
+               onAddBarang = { navController.navigate(DestinasAddBarang.route) },
+               onAddSuplier = { navController.navigate(DestinasiAddSuplier.route) },
+               onHomeBarang = { navController.navigate(DestinasiHomeBarang.route) },
+               onHomeSuplier = { navController.navigate(DestinasiHomeSuplier.route) },
+           )
+       }
+   }
 }
